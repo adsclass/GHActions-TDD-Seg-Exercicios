17daa050a85eec0d5e3b2691759a78daec9201a9
Walisson 23214290022 <walissonoficial19@gmail.com>
2024-10-07 20:50:43 -0300

    teste 2

--- src/math.js
+++ src/math.js
@@ -8,8 +8,13 @@ return resultado;
 }
 
 
-function fibonacci() {
-
+function fibonacci(w) {
+if (w < 0) return undefined; 
+const sequencia = [0,1];
+for (let i =2; i <= navigator; i++){
+  sequencia[i] = sequencia[i -1] + sequencia[i -2];
+}
+return sequencia.slice(0, w + 1);
 }
 function ehPrimo() {
 
