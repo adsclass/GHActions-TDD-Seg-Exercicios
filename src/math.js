83ee28f5a2bf1bbada88062bd9ab7c0e9593ae2d
Walisson 23214290022 <walissonoficial19@gmail.com>
2024-10-07 20:44:29 -0300

    teste1

--- src/math.js
+++ src/math.js
@@ -1,6 +1,13 @@
-function fatorial() {
-
+function fatorial(w) {
+if (w < 0) return undefined;
+if (w === 0) return 1;
+for (let i = 1; i <= w; i++) {
+  resultado *= i;
 }
+return resultado;
+}
+
+
 function fibonacci() {
 
 }
