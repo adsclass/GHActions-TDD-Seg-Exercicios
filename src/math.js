59bbc12178f83cb4c3b36c1f96cecc3319361c68
Vladimir Aires <vlad.aires05@gmail.com>
2024-10-07 21:45:01 -0300

    Inicio fatorial

--- src/math.js
+++ src/math.js
@@ -1,15 +1,32 @@
-function fatorial() {
+function fatorial(num) {
+    if (typeof num == "string") {
+        num = num - 0;
+    }
+    if (isNaN(num) || typeof num !== "number") {
+        throw new TypeError("O argumento deve ser um número válido");
+    }
 
-}
-function fibonacci() {
+    if (num < 0) {
+        throw new Error("Não existe fatorial de número negativo");
+    }
 
-}
-function ehPrimo() {
+    let resultado = 1;
 
+    if (num != 0) {
+        for (let i = 2; i <= num; i++) {
+            resultado = resultado * i;
+        }
+    } else {
+        resultado = 1;
+    }
+
+    return resultado;
 }
+function fibonacci() {}
+function ehPrimo() {}
 
 module.exports = {
-  fatorial,
-  fibonacci,
-  ehPrimo
-}
+    fatorial,
+    fibonacci,
+    ehPrimo,
+};
