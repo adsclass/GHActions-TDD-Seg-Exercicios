3b91d5f38180848e2aaed0f3d15ef7cd0a9225ff
Daniel <daniel.p.araujo@iesb.edu.br>
2024-10-09 20:00:23 -0300

    tests concluidos!

--- src/math.js
+++ src/math.js
@@ -1,44 +1,72 @@
 function fatorial(n) {
-  if (typeof n !== 'number' || n < 0) {
-    throw new Error("O argumento deve ser um número inteiro não negativo");
+ 
+  if (typeof n === "string") {
+    n = Number(n);
   }
-  if (n === 0) {
+
+  if (isNaN(n) || typeof n !== "number") {
+    throw new TypeError("O argumento deve ser um número válido");
+  }
+
+  if (n < 0) {
+    throw new Error("Não existe fatorial de número negativo");
+  }
+  if (n === 0 || n === 1) {
     return 1;
   }
   return n * fatorial(n - 1);
 }
 
 function fibonacci(n) {
-  if (typeof n !== 'number' || n <= 0) {
-    throw new Error("O argumento deve ser um número inteiro positivo");
+  if (typeof n === "string") {
+    n = Number(n);
   }
+  if (isNaN(n) || typeof n !== "number") {
+    throw new TypeError("O argumento deve ser um número válido");
+  }
+
+  if (n <= 0) {
+    throw new Error("Não existe fibonacci de número menor ou igual a zero");
+  }
+
   if (n === 1 || n === 2) {
     return 1;
   }
+  
   return fibonacci(n - 1) + fibonacci(n - 2);
 }
 
-function ehPrimo(num) {
-  if (typeof num !== 'number') {
-    throw new TypeError("O argumento deve ser um número válido");
-  } else if (num <= 1) {
-    throw new RangeError("O argumento deve ser um número inteiro maior que 1");
+function ehPrimo(n) {
+  if (typeof n === "string") {
+    n = Number(n);
   }
-  if (num <= 3) return true;
-  if (num % 2 === 0 || num % 3 === 0) return false;
+  if (isNaN(n) || typeof n !== "number") {
+    throw new TypeError("O argumento deve ser um número válido");
+  }
+ 
+  if (n <= 1) {
+    throw new Error("Não número primo de número menor ou igual a 1");
+  }
+  
+  if (n <= 3) {
+    return true;
+  }
+
+  if (n % 2 === 0 || n % 3 === 0) {
+    return false;
+  }
 
   let i = 5;
-  while (i * i <= num) {
-    if (num % i === 0 || num % (i + 2) === 0) return false;
+  while (i * i <= n) {
+    if (n % i === 0 || n % (i + 2) === 0) {
+      return false;
+    }
     i += 6;
   }
   return true;
-
 }
-
-
 module.exports = {
   fatorial,
   fibonacci,
-  ehPrimo
+  ehPrimo,
 };
