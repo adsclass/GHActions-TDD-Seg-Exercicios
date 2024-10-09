36a39d0c47fa894ff41d75eb365e5e02fb289fac
Eduardo Caetano <caetano151500@gmail.com>
2024-10-09 20:34:56 -0300

    function de fibonacci

--- src/math.js
+++ src/math.js
@@ -11,6 +11,24 @@ function fatorial(n) {
   return n * fatorial(n - 1);
 }
 
+function fibonacci(n) {
+  if (typeof n !== 'number' || isNaN(n)) {
+    throw new TypeError("O argumento deve ser um número válido");
+  }
+  if (n <= 0) {
+    throw new Error("Não existe fibonnacci de número menor ou igual a zero");
+  }
+  if (n === 1 || n === 2) {
+    return 1;
+  }
+  let a = 1, b = 1, temp;
+  for (let i = 3; i <= n; i++) {
+    temp = a + b;
+    a = b;
+    b = temp;
+  }
+  return b;
+}
 
 
-module.exports = { fatorial };
+module.exports = { fatorial, fibonacci };
