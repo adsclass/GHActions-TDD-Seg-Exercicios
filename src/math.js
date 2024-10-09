1a7cabf2b1ca5c869df045005ce61a8b5433fdbe
Eduardo Caetano <caetano151500@gmail.com>
2024-10-09 20:34:24 -0300

    function de fatorial

--- src/math.js
+++ src/math.js
@@ -1,15 +1,16 @@
-function fatorial() {
-
+function fatorial(n) {
+  if (typeof n !== 'number' || isNaN(n)) {
+    throw new TypeError("O argumento deve ser um número válido");
+  }
+  if (n < 0) {
+    throw new Error("Não existe fatorial de número negativo");
+  }
+  if (n === 0 || n === 1) {
+    return 1;
+  }
+  return n * fatorial(n - 1);
 }
-function fibonnacci() {
 
-}
-function ehPrimo() {
 
-}
 
-module.exports = {
-  fatorial,
-  fibonnacci,
-  ehPrimo
-}
+module.exports = { fatorial };
