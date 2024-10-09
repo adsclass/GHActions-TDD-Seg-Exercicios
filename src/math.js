09d8e20ea8957e2f43370d95b61c13bc4c5d12b6
Eduardo Caetano <caetano151500@gmail.com>
2024-10-09 20:35:20 -0300

    function de ehPrimo

--- src/math.js
+++ src/math.js
@@ -30,5 +30,19 @@ function fibonacci(n) {
   return b;
 }
 
+function ehPrimo(n) {
+  if (typeof n !== 'number' || isNaN(n)) {
+    throw new TypeError("O argumento deve ser um número válido");
+  }
+  if (n <= 1) {
+    throw new Error("Não número primo de número menor ou igual a 1");
+  }
+  for (let i = 2; i <= Math.sqrt(n); i++) {
+    if (n % i === 0) {
+      return false;
+    }
+  }
+  return true;
+}
 
-module.exports = { fatorial, fibonacci };
+module.exports = { fatorial, fibonacci, ehPrimo };
